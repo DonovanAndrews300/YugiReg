91522194db34105f5405a92ab47b6d2f4ecc9ec1
Donovan <donovanandrews300@gmail.com>
2024-06-10 11:43:11 -0400

    re-styled fle upload section to look nicer

--- client/src/components/FileUploader/FileUploader.tsx
+++ client/src/components/FileUploader/FileUploader.tsx
@@ -4,17 +4,18 @@ import { useDropzone } from 'react-dropzone';
 import { uploadFile } from '../../utils.js/uploadFile';
 
 export default function FileUploader() {
-  const [file, setFile] = useState<File|null>(null);
+  const [file, setFile] = useState<File | null>(null);
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [konamiId, setKonamiId] = useState('');
 
-  const onSubmit = async () => { 
+  const onSubmit = async () => {
     if (file) {
-      await uploadFile({file, firstName, lastName, konamiId});
+      await uploadFile({ file, firstName, lastName, konamiId });
       console.log('First Name:', firstName, 'Last Name:', lastName, 'Konami ID:', konamiId);
     }
   };
+
   const onDrop = useCallback((acceptedFiles: any[]) => {
     const isYdk = acceptedFiles.length && acceptedFiles[0].name.endsWith('.ydk');
     if (isYdk) {
@@ -43,62 +44,68 @@ export default function FileUploader() {
 
   return (
     <div className="layout-container">
-      <div className="text-inputs-container">
-        <div className="name-inputs">
-          <label>
-            First Name:
-            <input
-              type="text"
-              className="input"
-              name="firstName"
-              value={firstName}
-              onChange={handleInputChange}
-              placeholder="Enter your first name"
-            />
-          </label>
-          <label>
-            Last Name:
-            <input
-              type="text"
-              className="input"
-              name="lastName"
-              value={lastName}
-              onChange={handleInputChange}
-              placeholder="Enter your last name"
-            />
-          </label>
+      <div className="inputs-upload-container">
+        <div className="inputs-container">
+          <div className="name-inputs">
+            <label>
+              First Name:
+              <input
+                type="text"
+                className="input"
+                name="firstName"
+                value={firstName}
+                onChange={handleInputChange}
+                placeholder="Enter your first name"
+              />
+            </label>
+            <label>
+              Last Name:
+              <input
+                type="text"
+                className="input"
+                name="lastName"
+                value={lastName}
+                onChange={handleInputChange}
+                placeholder="Enter your last name"
+              />
+            </label>
+          </div>
+          <div className="konami-input">
+            <label>
+              Konami ID:
+              <input
+                type="text"
+                className="input"
+                name="konamiId"
+                value={konamiId}
+                onChange={handleInputChange}
+                maxLength={10}
+                placeholder="Enter your Konami ID"
+              />
+            </label>
+          </div>
         </div>
-        <div className="konami-input">
-          <label>
-            Konami ID:
-            <input
-              type="text"
-              className="input"
-              name="konamiId"
-              value={konamiId}
-              onChange={handleInputChange}
-              maxLength={10}
-              placeholder="Enter your Konami ID"
-            />
-          </label>
+        <div className="upload-container">
+          <div className="dropzone" {...getRootProps()}>
+            <input {...getInputProps()} />
+            {isDragActive ? <p>Release To Upload</p> : <p>Upload YDK File</p>}
+            {file?.name && (
+              <p className="file-name">{file.name}</p>
+            )}
+          </div>
+          <div className="buttons-container">
+            {file && (
+              <>
+                <button type="button" onClick={removeFile}>
+                  Remove
+                </button>
+                <button type="button" onClick={onSubmit}>
+                  Submit
+                </button>
+              </>
+            )}
+          </div>
         </div>
-      </div>
-      <div>
-        <div {...getRootProps({ className: "container" })}>
-          <input {...getInputProps({ className: "input" })} />
-          {isDragActive ? <p>Release To Upload</p> : <p>Upload YDK File</p>}
-        </div>
-        {file?.name && (
-          <>
-            <p>{file.name}</p>
-            <button type="button" onClick={removeFile}>
-              Remove
-            </button>
-            <button type="button" onClick={onSubmit}>
-              Submit
-            </button>
-          </>
-        )}
       </div>
     </div>
   );
