346012659f1a8815e2e3a8025d5eb279d7a29cd0
Donovan <donovanandrews300@gmail.com>
2024-06-11 17:34:56 -0400

    update instructions

--- client/src/Instructions/Instructions.tsx
+++ client/src/Instructions/Instructions.tsx
@@ -10,6 +10,7 @@ export default function Instructions() {
         <li>Retrieve the YDK file for your decklist.</li>
         <li>Drag and drop your YDK file into the white area above.</li>
         <li>You can alternatively click inside the white area to manually select your YDK file.</li>
+        <li>Fill out the Form and hit the submit button. Your file will automatically download.</li>
       </ol>
     </div>
   );
