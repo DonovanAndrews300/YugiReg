c13bb766797b11a6fd107ad0f6e54df0cc9ff7f2
Donovan <donovanandrews300@gmail.com>
2024-07-05 15:01:49 -0400

    uses heroku url for getting filters

--- client/src/utils/getFilters.ts
+++ client/src/utils/getFilters.ts
@@ -1,4 +1,4 @@
 export const getFilters = async () => {
-  const filters = await fetch("http://localhost:3000/filters");
+  const filters = await fetch("https://yugireg-45086852d588.herokuapp.com/filters");
   return filters;
 };
