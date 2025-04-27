b43b41681be773ba1e1ce30e4e4223b42cb35d21
Donovan <donovanandrews300@gmail.com>
2025-04-27 18:16:50 -0400

    added demo files to faq

--- client/src/components/Faq/Faq.tsx
+++ client/src/components/Faq/Faq.tsx
@@ -1,5 +1,4 @@
-
-
+import React from 'react';
 
 export default function Faq() {
   return (
@@ -10,13 +9,24 @@ export default function Faq() {
         <div>
           <h3 className='question-header'>Why don't some of my cards appear in the completed form?</h3>
           <p className='answer'>
-          Missing cards may occur because alternate card art is not currently supported.
+            Missing cards may occur because alternate card art is not currently supported.
           </p>
         </div>
         <div>
           <h3 className='question-header'>How can I obtain a YDK file of my deck?</h3>
           <p className='answer'>
-          You can obtain a YDK file of your deck by using a deck exporter. For Dueling Nexus, I recommend using this Chrome extension: [<a href="https://chrome.google.com/webstore/detail/duelingnexus-deck-exporte/aaloejogbofkmmonaddiaogjjjdjlpcd">Dueling Nexus Deck Exporter</a>].
+            You can obtain a YDK file of your deck by using a deck exporter. For Dueling Nexus, I recommend using this Chrome extension: [<a href="https://chrome.google.com/webstore/detail/duelingnexus-deck-exporte/aaloejogbofkmmonaddiaogjjjdjlpcd">Dueling Nexus Deck Exporter</a>].
+          </p>
+        </div>
+        <div>
+          <h3 className='question-header'>Can I try a demo before uploading my own deck?</h3>
+          <p className='answer'>
+            Yes! You can download demo files in the following formats:
+            <ul className="list-disc list-inside">
+              <li><a href="/demo.ydk" download>YDK Demo File</a></li>
+              <li><a href="/demo.xlsx" download>Excel Demo File</a></li>
+              <li><a href="/demo.csv" download>CSV Demo File</a></li>
+            </ul>
           </p>
         </div>
       </div>
